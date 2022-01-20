19e82f071f6fbb25b1b82dc33526c8093e8520d1
huynth1801 <huuhuy1801@gmail.com>
2022-01-20 20:27:22 +0700

    add graph

--- models/generator.py
+++ models/generator.py
@@ -2,7 +2,7 @@ from base_model import BaseModel
 from gc_layer import GatedConv2d, TransposeGatedConv2d
 import torch
 import torch.nn as nn
-# from torch_geometric.nn.conv import HypergraphConv
+from torch_geometric.nn.conv import HypergraphConv
 
 
 #-----------------------------------------------
@@ -77,12 +77,51 @@ class Generator(BaseModel):
 
         # Coarse out
         self.gated_conv18 = GatedConv2d(in_channels=64, 
-                                out_channels=3, kernel_size=3, stride=1, padding=1, dilation=1, pad_type='zero', activation='ELU')
+                            out_channels=3, kernel_size=3, stride=1, padding=1, dilation=1, pad_type='zero', activation='ELU')
 
 
         ##### Refine network #####
+        # Dau tien chung ta can concat 
+        self.refine1 = GatedConv2d(in_channels=4,
+                        out_channels=64, kernel_size=7, stride=1, dilation=1, padding=3, pad_type='zero', activation='ELU')
         
+        # Encoder for refine network
+        # H/2, W/2
+        self.refine2 = GatedConv2d(in_channels=64, out_channels=128, 
+                        kernel_size=3, stride=2, padding=1, dilation=1, pad_type='zero', activation='ELU')
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.refine3 = GatedConv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1,
+                        padding=1, dilation=1, pad_type='zero')
+        self.refine4 = GatedConv2d(in_channels=128, out_channels=128, kernel_size=3, stride=1,
+                        padding=1, dilation=1, pad_type='zero', activation='ELU')
+        self.refine5 = GatedConv2d(in_channels=128, out_channels=256, kernel_size=3,
+                            stride=2, padding=1, dilation=1, pad_type='zero', activation='ELU')
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.refine6 = GatedConv2d(in_channels=256, out_channels=256, kernel_size=3,
+                            stride=1, padding=1, dilation=1, pad_type='zero', activation='ELU')
+        self.refine7 = GatedConv2d(in_channels=256, out_channels=256, kernel_size=3,
+                            stride=1, padding=1, dilation=1,pad_type='zero', activation='ELU')
+        self.refine8 = GatedConv2d(in_channels=256, out_channels=512, kernel_size=3,
+                            stride=2, padding=1, dilation=1, pad_type='zero', activation='ELU')
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.refine9 = GatedConv2d(in_channels=512, out_channels=512, kernel_size=3, 
+                            stride=1, padding=2, dilation=2, pad_type='zero', activation='ELU')
+        self.refine10 = GatedConv2d(in_channels=512, out_channels=512, kernel_size=3,
+                            stride=1, padding=2, dilation=2, pad_type='zero', activation='ELU')
+        self.refine11 = GatedConv2d(in_channels=512, out_channels=512, kernel_size=3,
+                            stride=1, padding=2, dilation=2, pad_type='zero', activation='ELU')
+        self.refine12 = GatedConv2d(in_channels=512, out_channels=1024, kernel_size=3,
+                            stride=1, padding=1, dilation=1, pad_type='zero', activation='ELU')
+        self.refine13 = GatedConv2d(in_channels=1024, out_channels=1024, kernel_size=3,
+                            stride=1, padding=1, dilation=1, pad_type='zero', activation='ELU')
+        self.refine14 = GatedConv2d(in_channels=1024, out_channels=1024, kernel_size=3,
+                            stride=1, padding=1, dilation=1, pad_type='zero', activation='ELU')
 
+        # Apply Hypergraph convolution on last skip connections
+        self.graph1 = HypergraphConv(in_channels=256, out_channels=512, num_edges=128, training=True)
+        self.elu_g1 = nn.ELU()
+        self.graph2 = HypergraphConv(in_channels=128, out_channels=128, num_edges=128, training=True)
+        self.elu_g2 = nn.ELU()
 
 
 
